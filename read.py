1970fddd8a387e26f6eaa9e98baeb2c8077a70bb
Markus Springer <markus.springer1994@googlemail.com>
2017-09-28 19:44:35 +0200

    Updated Read.py to get unique id of NFC Card

--- read.py
+++ read.py
@@ -1,10 +1,40 @@
 #!/usr/bin/env python
-import os
-import sys
+# -*- coding: utf8 -*-
 
+import RPi.GPIO as GPIO
+import MFRC522
+import signal
 
-for line in sys.stdin:
-  test = line[:-1]
+continue_reading = True
 
+# Capture SIGINT for cleanup when the script is aborted
+def end_read(signal,frame):
+    global continue_reading
+    print "Ctrl+C captured, ending read."
+    continue_reading = False
+    GPIO.cleanup()
 
-print "ischisch"
+# Hook the SIGINT
+signal.signal(signal.SIGINT, end_read)
+
+# Create an object of the class MFRC522
+MIFAREReader = MFRC522.MFRC522()
+
+# Welcome message
+
+# This loop keeps checking for chips. If one is near it will get the UID and authenticate
+while continue_reading:
+
+    # Scan for cards
+    (status,TagType) = MIFAREReader.MFRC522_Request(MIFAREReader.PICC_REQIDL)
+
+    # Get the UID of the card
+    (status,uid) = MIFAREReader.MFRC522_Anticoll()
+
+    # If we have the UID, continue
+    if status == MIFAREReader.MI_OK:
+
+        # Print UID
+        print ''.join(str(e) for e in uid)
+        GPIO.cleanup()
+        continue_reading = False
